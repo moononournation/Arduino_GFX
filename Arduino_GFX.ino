3ff62e2b07eabefa4b0523d8a199d0fe8aefcbcf
moononournation <moononournation@gmail.com>
2019-09-22 21:02:11 +0800

    typo

--- Arduino_GFX.ino
+++ Arduino_GFX.ino
@@ -742,14 +742,14 @@ uint32_t testRoundRects()
 
   This is an example sketch for the Adafruit 2.2" SPI display.
   This library works with the Adafruit 2.2" TFT Breakout w/SD card
-  ----> http://www.adafruit.com/productsb/1480
+  ----> http://www.adafruit.com/products/1480
 
   Check out the links above for our tutorials and wiring diagrams
   These displays use SPI to communicate, 4 or 5 pins are required to
   interface (RST is optional)
-  Adafruit investsb time and resources providing this open source code,
+  Adafruit invests time and resources providing this open source code,
   please support Adafruit and open-source hardware by purchasing
-  productsb from Adafruit!
+  products from Adafruit!
 
   Written by Limor Fried/Ladyada for Adafruit Industries.
   MIT license, all text above must be included in any redistribution
