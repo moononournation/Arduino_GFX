3f0c5724cf534fd6c1e5557ada0242fa52f1ca11
moononournation <moononournation@gmail.com>
2020-03-01 15:51:35 +0800

    add ESP32PhotoFrame example

---
+++ examples/ESP32PhotoFrame/ESP32PhotoFrame.ino
@@ -0,0 +1,323 @@
+/*******************************************************************************
+ * ESP32 Photo Frame
+ * This is a simple IoT photo frame sample
+ * Please find more details at instructables:
+ * https://www.instructables.com/id/Face-Aware-OSD-Photo-Frame/
+ * 
+ * Setup steps:
+ * 1. Fill your own SSID_NAME, SSID_PASSWORD and URL_TEMPLATE
+ * 2. Change your LCD parameters in Arduino_GFX setting
+ ******************************************************************************/
+
+/* WiFi settings */
+#define SSID_NAME "YourAP"
+#define SSID_PASSWORD "PleaseInputYourPasswordHere"
+
+#define TFT_ROTATION 1 // landscape
+#define URL_TEMPLATE "http://YourServerNameOrIP:8080/?w=%d&h=%d"
+
+#define HTTP_TIMEOUT 60000 // in ms, wait a while for server processing
+
+// WDT, enable auto restart from unexpected hang, it may caused by JPEG decode
+#define ENABLE_WDT
+
+/*******************************************************************************
+ * Start of Arduino_GFX setting
+ ******************************************************************************/
+#include "SPI.h"
+#include "Arduino_HWSPI.h"
+#include "Arduino_ESP32SPI.h"
+#include "Arduino_SWSPI.h"
+#include "Arduino_GFX.h"            // Core graphics library
+#include "Arduino_Canvas.h"         // Canvas (framebuffer) library
+#include "Arduino_Canvas_Indexed.h" // Indexed Color Canvas (framebuffer) library
+#include "Arduino_HX8347C.h"        // Hardware-specific library for HX8347C
+#include "Arduino_HX8352C.h"        // Hardware-specific library for HX8352C
+#include "Arduino_HX8357B.h"        // Hardware-specific library for HX8357B
+#include "Arduino_ILI9225.h"        // Hardware-specific library for ILI9225
+#include "Arduino_ILI9341.h"        // Hardware-specific library for ILI9341
+#include "Arduino_ILI9481_18bit.h"  // Hardware-specific library for ILI9481
+#include "Arduino_ILI9486_18bit.h"  // Hardware-specific library for ILI9486
+#include "Arduino_SEPS525.h"        // Hardware-specific library for SEPS525
+#include "Arduino_SSD1283A.h"       // Hardware-specific library for SSD1283A
+#include "Arduino_SSD1331.h"        // Hardware-specific library for SSD1331
+#include "Arduino_SSD1351.h"        // Hardware-specific library for SSD1351
+#include "Arduino_ST7735.h"         // Hardware-specific library for ST7735
+#include "Arduino_ST7789.h"         // Hardware-specific library for ST7789
+#include "Arduino_ST7796.h"         // Hardware-specific library for ST7796
+
+#if defined(ARDUINO_M5Stack_Core_ESP32) || defined(ARDUINO_M5STACK_FIRE)
+#define TFT_BL 32
+#include "Arduino_ILI9341_M5STACK.h"
+Arduino_ESP32SPI *bus = new Arduino_ESP32SPI(27 /* DC */, 14 /* CS */, SCK, MOSI, MISO);
+Arduino_ILI9341_M5STACK *gfx = new Arduino_ILI9341_M5STACK(bus, 33 /* RST */, 1 /* rotation */);
+#elif defined(ARDUINO_ODROID_ESP32)
+#define TFT_BL 14
+Arduino_ESP32SPI *bus = new Arduino_ESP32SPI(21 /* DC */, 5 /* CS */, SCK, MOSI, MISO);
+Arduino_ILI9341 *gfx = new Arduino_ILI9341(bus, -1 /* RST */, 3 /* rotation */);
+// Arduino_ST7789 *gfx = new Arduino_ST7789(bus,  -1 /* RST */, 1 /* rotation */, true /* IPS */);
+#elif defined(ARDUINO_T) // TTGO T-Watch
+#define TFT_BL 12
+Arduino_ESP32SPI *bus = new Arduino_ESP32SPI(27 /* DC */, 5 /* CS */, 18 /* SCK */, 19 /* MOSI */, -1 /* MISO */);
+Arduino_ST7789 *gfx = new Arduino_ST7789(bus, -1 /* RST */, 2 /* rotation */, true /* IPS */, 240, 240, 0, 80);
+#else /* not a specific hardware */
+
+#if defined(ESP32)
+#define TFT_CS 5
+// #define TFT_CS -1 // for display without CS pin
+// #define TFT_DC 16
+#define TFT_DC 27
+// #define TFT_DC -1 // for display without DC pin (9-bit SPI)
+// #define TFT_RST 17
+#define TFT_RST 33
+#define TFT_BL 22
+#elif defined(ESP8266)
+#define TFT_CS 15
+#define TFT_DC 5
+#define TFT_RST -1
+// #define TFT_BL 4
+#else
+#define TFT_CS 20
+#define TFT_DC 19
+#define TFT_RST 18
+#define TFT_BL 10
+#endif
+
+/*
+ * Step 1: Initize one databus for your display
+*/
+
+// General software SPI
+// Arduino_DataBus *bus = new Arduino_SWSPI(TFT_DC, TFT_CS, 18 /* SCK */, 23 /* MOSI */, -1 /* MISO */);
+
+// General hardware SPI
+Arduino_DataBus *bus = new Arduino_HWSPI(TFT_DC, TFT_CS);
+
+// ESP32 hardware SPI, more customizable parameters
+// Arduino_DataBus *bus = new Arduino_ESP32SPI(TFT_DC, TFT_CS, 18 /* SCK */, 23 /* MOSI */, -1 /* MISO */, VSPI /* spi_num */);
+
+/*
+ * Step 2: Initize one driver for your display
+*/
+
+// Canvas (framebuffer)
+// Arduino_ST7789 *output_display = new Arduino_ST7789(bus, TFT_RST, 0 /* rotation */, true /* IPS */);
+// 16-bit color Canvas (240x320 resolution only works for ESP32 with PSRAM)
+// Arduino_Canvas *gfx = new Arduino_Canvas(240, 320, output_display);
+// Indexed color Canvas, mask_level: 0-2, larger mask level mean less color variation but can have faster index mapping
+// Arduino_Canvas_Indexed *gfx = new Arduino_Canvas_Indexed(240, 320, output_display, MAXMASKLEVEL /* mask_level */);
+
+// HX8347C IPS LCD 240x320
+// Arduino_HX8347C *gfx = new Arduino_HX8347C(bus, TFT_RST, 0 /* rotation */, true /* IPS */);
+
+// HX8352C IPS LCD 240x400
+// Arduino_HX8352C *gfx = new Arduino_HX8352C(bus, TFT_RST, 0 /* rotation */, true /* IPS */);
+
+// HX8357B IPS LCD 320x480
+// Arduino_HX8357B *gfx = new Arduino_HX8357B(bus, TFT_RST, 0 /* rotation */, true /* IPS */);
+
+// ILI9225 LCD 176x220
+// Arduino_ILI9225 *gfx = new Arduino_ILI9225(bus, TFT_RST);
+
+// ILI9341 LCD 240x320
+Arduino_ILI9341 *gfx = new Arduino_ILI9341(bus, TFT_RST);
+
+// ILI9481 LCD 320x480
+// Arduino_ILI9481_18bit *gfx = new Arduino_ILI9481_18bit(bus, TFT_RST);
+
+// ILI9486 LCD 320x480
+// Arduino_ILI9486_18bit *gfx = new Arduino_ILI9486_18bit(bus, TFT_RST);
+
+// SEPS525 OLED 160x128
+// Arduino_SEPS525 *gfx = new Arduino_SEPS525(bus, TFT_RST);
+
+// SSD1283A OLED 130x130
+// Arduino_SSD1283A *gfx = new Arduino_SSD1283A(bus, TFT_RST);
+
+// SSD1331 OLED 96x64
+// Arduino_SSD1331 *gfx = new Arduino_SSD1331(bus, TFT_RST);
+
+// SSD1351 OLED 128x128
+// Arduino_SSD1351 *gfx = new Arduino_SSD1351(bus, TFT_RST);
+
+// ST7735 LCD
+// 1.8" REDTAB 128x160
+// Arduino_ST7735 *gfx = new Arduino_ST7735(bus, TFT_RST);
+// 1.8" BLACKTAB 128x160
+// Arduino_ST7735 *gfx = new Arduino_ST7735(bus, TFT_RST, 0 /* rotation */, false /* IPS */, 128 /* width */, 160 /* height */, 2 /* col offset 1 */, 1 /* row offset 1 */, 2 /* col offset 2 */, 1 /* row offset 2 */, false /* BGR */);
+// 1.8" GREENTAB A 128x160
+// Arduino_ST7735 *gfx = new Arduino_ST7735(bus, TFT_RST, 0 /* rotation */, false /* IPS */, 128 /* width */, 160 /* height */, 2 /* col offset 1 */, 1 /* row offset 1 */, 2 /* col offset 2 */, 1 /* row offset 2 */);
+// 1.8" GREENTAB B 128x160
+// Arduino_ST7735 *gfx = new Arduino_ST7735(bus, TFT_RST, 0 /* rotation */, false /* IPS */, 128 /* width */, 160 /* height */, 2 /* col offset 1 */, 3 /* row offset 1 */, 2 /* col offset 2 */, 1 /* row offset 2 */);
+// 1.8" Wide angle LCD 128x160
+// Arduino_ST7735 *gfx = new Arduino_ST7735(bus, TFT_RST, 2 /* rotation */, false /* IPS */, 128 /* width */, 160 /* height */, 0 /* col offset 1 */, 0 /* row offset 1 */, 0 /* col offset 2 */, 0 /* row offset 2 */, false /* BGR */);
+// 1.5" GREENTAB B 128x128
+// Arduino_ST7735 *gfx = new Arduino_ST7735(bus, TFT_RST, 0 /* rotation */, false /* IPS */, 128 /* width */, 128 /* height */, 2 /* col offset 1 */, 3 /* row offset 1 */, 2 /* col offset 2 */, 1 /* row offset 2 */);
+// 1.5" GREENTAB C 128x128
+// Arduino_ST7735 *gfx = new Arduino_ST7735(bus, TFT_RST, 0 /* rotation */, false /* IPS */, 128 /* width */, 128 /* height */, 0 /* col offset 1 */, 32 /* row offset 1 */);
+// 0.96" IPS LCD 80x160
+// Arduino_ST7735 *gfx = new Arduino_ST7735(bus, TFT_RST, 3 /* rotation */, true /* IPS */, 80 /* width */, 160 /* height */, 26 /* col offset 1 */, 1 /* row offset 1 */, 26 /* col offset 2 */, 1 /* row offset 2 */);
+
+// ST7789 LCD
+// 2.4" LCD 240x320
+// Arduino_ST7789 *gfx = new Arduino_ST7789(bus, TFT_RST);
+// 2.4" IPS LCD 240x320
+// Arduino_ST7789 *gfx = new Arduino_ST7789(bus, TFT_RST, 0 /* rotation */, true /* IPS */);
+// 1.3"/1.5" square IPS LCD 240x240
+// Arduino_ST7789 *gfx = new Arduino_ST7789(bus, TFT_RST, 2 /* rotation */, true /* IPS */, 240 /* width */, 240 /* height */, 0 /* col offset 1 */, 80 /* row offset 1 */);
+// 1.14" IPS LCD 135x240 TTGO T-Display
+// Arduino_ST7789 *gfx = new Arduino_ST7789(bus, TFT_RST, 0 /* rotation */, true /* IPS */, 135 /* width */, 240 /* height */, 53 /* col offset 1 */, 40 /* row offset 1 */, 52 /* col offset 2 */, 40 /* row offset 2 */);
+
+// ST7796 LCD
+// 4" LCD 320x480
+// Arduino_ST7796 *gfx = new Arduino_ST7796(bus, TFT_RST);
+// 4" IPS LCD 320x480
+// Arduino_ST7796 *gfx = new Arduino_ST7796(bus, TFT_RST, 0 /* rotation */, true /* IPS */);
+
+#endif /* not a specific hardware */
+/*******************************************************************************
+ * End of Arduino_GFX setting
+ ******************************************************************************/
+
+#include <WiFi.h>
+#include <esp_jpg_decode.h>
+#include <esp_task_wdt.h>
+#include <SPI.h>
+#include <HTTPClient.h>
+
+static int len, offset;
+static unsigned long next_show_millis = 0;
+
+HTTPClient http;
+char url[1024];
+
+void setup()
+{
+  Serial.begin(115200);
+  gfx->begin();
+  gfx->fillScreen(BLACK);
+
+  // setup url query value with LCD dimension
+  sprintf(url, URL_TEMPLATE, gfx->width(), gfx->height());
+
+  WiFi.begin(SSID_NAME, SSID_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" CONNECTED");
+
+#ifdef TFT_BL
+  pinMode(TFT_BL, OUTPUT);
+  digitalWrite(TFT_BL, HIGH);
+#endif
+
+#ifdef ENABLE_WDT
+  // set WDT timeout a little bit longer than HTTP timeout
+  esp_task_wdt_init((HTTP_TIMEOUT / 1000) + 1, true);
+  enableLoopWDT();
+#endif
+}
+
+void loop()
+{
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    // wait for WiFi connection
+    delay(500);
+  }
+  else if (millis() < next_show_millis) {
+    delay(1000);
+  }
+  else
+  {
+    next_show_millis = ((millis() / 60000L) + 1) * 60000L; // next minute
+    Serial.print("[HTTP] begin...\n");
+    http.begin(url);
+    http.setTimeout(HTTP_TIMEOUT);
+    Serial.print("[HTTP] GET...\n");
+    int httpCode = http.GET();
+
+    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+    // HTTP header has been send and Server response header has been handled
+    if (httpCode <= 0)
+    {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+    else
+    {
+      if (httpCode != HTTP_CODE_OK)
+      {
+        Serial.printf("[HTTP] Not OK!\n");
+        delay(5000);
+      }
+      else
+      {
+        // get lenght of document (is -1 when Server sends no Content-Length header)
+        len = http.getSize();
+        Serial.printf("[HTTP] size: %d\n", len);
+
+        if (len <= 0)
+        {
+          Serial.printf("[HTTP] Unknow content size: %d\n", len);
+        }
+        else
+        {
+          // get tcp stream
+          WiFiClient *http_stream = http.getStreamPtr();
+
+          esp_jpg_decode(len, JPG_SCALE_NONE, http_stream_reader, tft_writer, http_stream /* arg */);
+        }
+      }
+    }
+    http.end();
+  }
+
+#ifdef ENABLE_WDT
+  // notify WDT still working
+  feedLoopWDT();
+#endif
+}
+
+static size_t http_stream_reader(void *arg, size_t index, uint8_t *buf, size_t len)
+{
+  WiFiClient *http_stream = (WiFiClient *)arg;
+  if (buf)
+  {
+    // Serial.printf("[HTTP] read: %d\n", len);
+    size_t a = http_stream->available();
+    size_t r = 0;
+    while (r < len)
+    {
+      r += http_stream->readBytes(buf + r, min((len - r), a));
+    }
+    return r;
+  }
+  else
+  {
+    // Serial.printf("[HTTP] skip: %d\n", len);
+    int l = len;
+    while (l--)
+    {
+      http_stream->read();
+    }
+    return len;
+  }
+}
+
+static bool tft_writer(void *arg, uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8_t *data)
+{
+  if (data)
+  {
+    // Serial.printf("%d, %d, %d, %d\n", x, y, w, h);
+    gfx->draw24bitRGBBitmap(x, y, data, w, h);
+  }
+
+#ifdef ENABLE_WDT
+  // notify WDT still working
+  feedLoopWDT();
+#endif
+
+  return true; // Continue to decompression
+}
