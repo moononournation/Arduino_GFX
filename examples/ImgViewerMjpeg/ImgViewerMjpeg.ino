8a6cb7b4f6a89cf2e59bbf03904235c4b59514ab
moononournation <moononournation@gmail.com>
2020-12-08 18:37:24 +0800

    add ImgViewerMjpeg example

---
+++ examples/ImgViewerMjpeg/ImgViewerMjpeg.ino
@@ -0,0 +1,325 @@
+/*******************************************************************************
+ * Motion JPEG Viewer
+ * This is a simple Motion JPEG image viewer example
+ * Image Source: https://www.pexels.com/video/earth-rotating-video-856356/
+ * cropped: x: 598 y: 178 width: 720 height: 720 resized: 240x240
+ * ffmpeg -i "Pexels Videos 3931.mp4" -ss 0 -t 20.4s -vf "reverse,setpts=0.5*PTS,fps=10,vflip,hflip,rotate=90,crop=720:720:178:598,scale=240:240:flags=lanczos" -pix_fmt yuv420p -q:v 9 earth.mjpeg
+ *
+ * Dependent libraries:
+ * JPEGDEC: https://github.com/bitbank2/JPEGDEC.git
+ * 
+ * Setup steps:
+ * 1. Change your LCD parameters in Arduino_GFX setting
+ * 2. Upload Motion JPEG file
+ *   SPIFFS (ESP8266 / ESP32):
+ *     upload SPIFFS data with ESP32 Sketch Data Upload:
+ *     ESP8266: https://github.com/esp8266/arduino-esp8266fs-plugin
+ *     ESP32: https://github.com/me-no-dev/arduino-esp32fs-plugin
+ *   SD:
+ *     Most Arduino system built-in support SD file system.
+ *     Wio Terminal require extra dependant Libraries:
+ *     - Seeed_Arduino_FS: https://github.com/Seeed-Studio/Seeed_Arduino_FS.git
+ *     - Seeed_Arduino_SFUD: https://github.com/Seeed-Studio/Seeed_Arduino_SFUD.git
+ ******************************************************************************/
+#define MJPEG_FILENAME "/earth.mjpeg"
+#define MJPEG_BUFFER_SIZE (240 * 240 * 2 / 10) // memory for a single JPEG frame
+
+/*******************************************************************************
+ * Start of Arduino_GFX setting
+ ******************************************************************************/
+#include "Arduino_GFX_Library.h"
+
+/* first check if selected specific hardware */
+// #define ESP32_LCDKIT_SPI
+#if defined(ESP32_LCDKIT_SPI)
+#define TFT_BL 23
+Arduino_DataBus *bus = new Arduino_ESP32SPI(19 /* DC */, 5 /* CS */, 22 /* SCK */, 21 /* MOSI */, 27 /* MISO */);
+Arduino_ILI9341 *gfx = new Arduino_ILI9341(bus, 18 /* RST */, 1 /* rotation */);
+
+/* Wio Terminal */
+#elif defined(ARDUINO_ARCH_SAMD) && defined(SEEED_GROVE_UI_WIRELESS)
+#define TFT_BL LCD_BACKLIGHT
+Arduino_HWSPI *bus = new Arduino_HWSPI(LCD_DC /* DC */, LCD_SS_PIN /* CS */);
+Arduino_ILI9341 *gfx = new Arduino_ILI9341(bus, -1 /* RST */, 3 /* rotation */);
+
+/* M5Stack */
+#elif defined(ARDUINO_M5Stack_Core_ESP32) || defined(ARDUINO_M5STACK_FIRE)
+#define TFT_BL 32
+Arduino_ESP32SPI *bus = new Arduino_ESP32SPI(27 /* DC */, 14 /* CS */, SCK, MOSI, MISO);
+Arduino_ILI9341_M5STACK *gfx = new Arduino_ILI9341_M5STACK(bus, 33 /* RST */, 1 /* rotation */);
+
+/* Odroid-Go */
+#elif defined(ARDUINO_ODROID_ESP32)
+#define TFT_BL 14
+Arduino_ESP32SPI *bus = new Arduino_ESP32SPI(21 /* DC */, 5 /* CS */, SCK, MOSI, MISO);
+Arduino_ILI9341 *gfx = new Arduino_ILI9341(bus, -1 /* RST */, 3 /* rotation */);
+// Arduino_ST7789 *gfx = new Arduino_ST7789(bus,  -1 /* RST */, 1 /* rotation */, true /* IPS */);
+
+/* TTGO T-Watch */
+#elif defined(ARDUINO_T) || defined(ARDUINO_TWATCH_BASE) || defined(ARDUINO_TWATCH_2020_V1) || defined(ARDUINO_TWATCH_2020_V2)
+#define TFT_BL 12
+Arduino_ESP32SPI *bus = new Arduino_ESP32SPI(27 /* DC */, 5 /* CS */, 18 /* SCK */, 19 /* MOSI */, -1 /* MISO */);
+Arduino_ST7789 *gfx = new Arduino_ST7789(bus, -1 /* RST */, 2 /* rotation */, true /* IPS */, 240, 240, 0, 80);
+
+#else /* not selected specific hardware */
+
+#if defined(ESP32)
+#define TFT_CS 5
+// #define TFT_CS -1 // for display without CS pin
+// #define TFT_DC 16
+#define TFT_DC 27
+// #define TFT_DC -1 // for display without DC pin (9-bit SPI)
+// #define TFT_RST 17
+#define TFT_RST 33
+#define TFT_BL 22
+#elif defined(ESP8266)
+#define TFT_CS 15
+#define TFT_DC 5
+#define TFT_RST -1
+// #define TFT_BL 4
+#else
+#define TFT_CS 20
+#define TFT_DC 19
+#define TFT_RST 18
+#define TFT_BL 10
+#endif
+
+/*
+ * Step 1: Initize one databus for your display
+*/
+
+// General software SPI
+// Arduino_DataBus *bus = new Arduino_SWSPI(TFT_DC, TFT_CS, 18 /* SCK */, 23 /* MOSI */, -1 /* MISO */);
+
+// General hardware SPI
+Arduino_DataBus *bus = new Arduino_HWSPI(TFT_DC, TFT_CS);
+
+// ESP32 hardware SPI, more customizable parameters
+// Arduino_DataBus *bus = new Arduino_ESP32SPI(TFT_DC, TFT_CS, 18 /* SCK */, 23 /* MOSI */, -1 /* MISO */, VSPI /* spi_num */);
+
+// ESP32 parallel 8-bit
+// Arduino_DataBus *bus = new Arduino_ESP32PAR8(TFT_DC, TFT_CS, 26 /* WR */, -1 /* RD */, 16 /* D0 */, 17 /* D1 */, 18 /* D2 */, 19 /* D3 */, 21 /* D4 */, 12 /* D5 */, 23 /* D6 */, 25 /* D7 */);
+
+// ESP32 parallel 16-bit
+// Almost all GPIO 0-31 used up for 16-bit and WR, disable PSRAM to gain 16 and 17 but still no GPIOs remain for CS and RD.
+// CS connect to GND (enable); RD connect to Vcc (disable).
+// Arduino_DataBus *bus = new Arduino_ESP32PAR16(
+//     32 /* DC */, -1 /* CS */, 21 /* WR */, -1 /* RD */,
+//     19 /* D0 */, 23 /* D1 */, 18 /* D2 */, 5 /* D3 */, 17 /* D4 */, 16 /* D5 */, 25 /* D6 */, 26 /* D7 */,
+//     27 /* D8 */, 14 /* D9 */, 12 /* D10 */, 13 /* D11 */, 15 /* D12 */, 2 /* D13 */, 0 /* D14 */, 4 /* D15 */);
+
+/*
+ * Step 2: Initize one driver for your display
+*/
+
+// Canvas (framebuffer)
+// Arduino_ST7789 *output_display = new Arduino_ST7789(bus, TFT_RST, 0 /* rotation */, true /* IPS */);
+// 16-bit color Canvas (240x320 resolution only works for ESP32 with PSRAM)
+// Arduino_Canvas *gfx = new Arduino_Canvas(240 /* width */, 320 /* height */, output_display);
+// Indexed color Canvas, mask_level: 0-2, larger mask level mean less color variation but can have faster index mapping
+// Arduino_Canvas_Indexed *gfx = new Arduino_Canvas_Indexed(240 /* width */, 320 /* height */, output_display, 0 /* output_x */, 0 /* output_y */, MAXMASKLEVEL /* mask_level */);
+
+// GC9A01 IPS LCD 240x240
+// Arduino_GC9A01 *gfx = new Arduino_GC9A01(bus, TFT_RST, 0 /* rotation */, true /* IPS */);
+
+// HX8347C IPS LCD 240x320
+// Arduino_HX8347C *gfx = new Arduino_HX8347C(bus, TFT_RST, 0 /* rotation */, true /* IPS */);
+
+// HX8347D IPS LCD 240x320
+// Arduino_HX8347D *gfx = new Arduino_HX8347D(bus, TFT_RST, 0 /* rotation */, true /* IPS */);
+
+// HX8352C IPS LCD 240x400
+// Arduino_HX8352C *gfx = new Arduino_HX8352C(bus, TFT_RST, 0 /* rotation */, true /* IPS */);
+
+// HX8357B IPS LCD 320x480
+// Arduino_HX8357B *gfx = new Arduino_HX8357B(bus, TFT_RST, 0 /* rotation */, true /* IPS */);
+
+// ILI9225 LCD 176x220
+// Arduino_ILI9225 *gfx = new Arduino_ILI9225(bus, TFT_RST);
+
+// ILI9341 LCD 240x320
+Arduino_ILI9341 *gfx = new Arduino_ILI9341(bus, TFT_RST, 0 /* rotation */);
+
+// ILI9481 LCD 320x480
+// Arduino_ILI9481_18bit *gfx = new Arduino_ILI9481_18bit(bus, TFT_RST, 0 /* rotation */);
+
+// ILI9486 LCD 320x480
+// Arduino_ILI9486_18bit *gfx = new Arduino_ILI9486_18bit(bus, TFT_RST, 0 /* rotation */);
+
+// ILI9488 LCD 320x480
+// Arduino_ILI9488_18bit *gfx = new Arduino_ILI9488_18bit(bus, TFT_RST, 0 /* rotation */);
+
+// JBT6K71 LCD 240x320
+// Arduino_JBT6K71 *gfx = new Arduino_JBT6K71(bus, TFT_RST, 0 /* rotation */, true /* IPS */, 240, 320, 0, 0, 16, 0);
+
+// R61529 IPS LCD 320x480
+// Arduino_R61529 *gfx = new Arduino_R61529(bus, TFT_RST, 0 /* rotation */, true /* IPS */);
+
+// SEPS525 OLED 160x128
+// Arduino_SEPS525 *gfx = new Arduino_SEPS525(bus, TFT_RST, 0 /* rotation */);
+
+// SSD1283A OLED 130x130
+// Arduino_SSD1283A *gfx = new Arduino_SSD1283A(bus, TFT_RST, 0 /* rotation */);
+
+// SSD1331 OLED 96x64
+// Arduino_SSD1331 *gfx = new Arduino_SSD1331(bus, TFT_RST, 0 /* rotation */);
+
+// SSD1351 OLED 128x128
+// Arduino_SSD1351 *gfx = new Arduino_SSD1351(bus, TFT_RST, 0 /* rotation */);
+
+// ST7735 LCD
+// 1.8" REDTAB 128x160
+// Arduino_ST7735 *gfx = new Arduino_ST7735(bus, TFT_RST, 0 /* rotation */);
+// 1.8" BLACKTAB 128x160
+// Arduino_ST7735 *gfx = new Arduino_ST7735(bus, TFT_RST, 0 /* rotation */, false /* IPS */, 128 /* width */, 160 /* height */, 2 /* col offset 1 */, 1 /* row offset 1 */, 2 /* col offset 2 */, 1 /* row offset 2 */, false /* BGR */);
+// 1.8" GREENTAB A 128x160
+// Arduino_ST7735 *gfx = new Arduino_ST7735(bus, TFT_RST, 0 /* rotation */, false /* IPS */, 128 /* width */, 160 /* height */, 2 /* col offset 1 */, 1 /* row offset 1 */, 2 /* col offset 2 */, 1 /* row offset 2 */);
+// 1.8" GREENTAB B 128x160
+// Arduino_ST7735 *gfx = new Arduino_ST7735(bus, TFT_RST, 0 /* rotation */, false /* IPS */, 128 /* width */, 160 /* height */, 2 /* col offset 1 */, 3 /* row offset 1 */, 2 /* col offset 2 */, 1 /* row offset 2 */);
+// 1.8" Wide angle LCD 128x160
+// Arduino_ST7735 *gfx = new Arduino_ST7735(bus, TFT_RST, 2 /* rotation */, false /* IPS */, 128 /* width */, 160 /* height */, 0 /* col offset 1 */, 0 /* row offset 1 */, 0 /* col offset 2 */, 0 /* row offset 2 */, false /* BGR */);
+// 1.5" GREENTAB B 128x128
+// Arduino_ST7735 *gfx = new Arduino_ST7735(bus, TFT_RST, 0 /* rotation */, false /* IPS */, 128 /* width */, 128 /* height */, 2 /* col offset 1 */, 3 /* row offset 1 */, 2 /* col offset 2 */, 1 /* row offset 2 */);
+// 1.5" GREENTAB C 128x128
+// Arduino_ST7735 *gfx = new Arduino_ST7735(bus, TFT_RST, 0 /* rotation */, false /* IPS */, 128 /* width */, 128 /* height */, 0 /* col offset 1 */, 32 /* row offset 1 */);
+// 0.96" IPS LCD 80x160
+// Arduino_ST7735 *gfx = new Arduino_ST7735(bus, TFT_RST, 3 /* rotation */, true /* IPS */, 80 /* width */, 160 /* height */, 26 /* col offset 1 */, 1 /* row offset 1 */, 26 /* col offset 2 */, 1 /* row offset 2 */);
+
+// ST7789 LCD
+// 2.4" LCD 240x320
+// Arduino_ST7789 *gfx = new Arduino_ST7789(bus, TFT_RST, 0 /* rotation */;
+// 2.4" IPS LCD 240x320
+// Arduino_ST7789 *gfx = new Arduino_ST7789(bus, TFT_RST, 0 /* rotation */, true /* IPS */);
+// 1.3"/1.5" square IPS LCD 240x240
+// Arduino_ST7789 *gfx = new Arduino_ST7789(bus, TFT_RST, 2 /* rotation */, true /* IPS */, 240 /* width */, 240 /* height */, 0 /* col offset 1 */, 80 /* row offset 1 */);
+// 1.14" IPS LCD 135x240 TTGO T-Display
+// Arduino_ST7789 *gfx = new Arduino_ST7789(bus, TFT_RST, 0 /* rotation */, true /* IPS */, 135 /* width */, 240 /* height */, 53 /* col offset 1 */, 40 /* row offset 1 */, 52 /* col offset 2 */, 40 /* row offset 2 */);
+
+// ST7796 LCD
+// 4" LCD 320x480
+// Arduino_ST7796 *gfx = new Arduino_ST7796(bus, TFT_RST, 0 /* rotation */);
+// 4" IPS LCD 320x480
+// Arduino_ST7796 *gfx = new Arduino_ST7796(bus, TFT_RST, 0 /* rotation */, true /* IPS */);
+
+#endif /* not selected specific hardware */
+/*******************************************************************************
+ * End of Arduino_GFX setting
+ ******************************************************************************/
+
+/* Wio Terminal */
+#if defined(ARDUINO_ARCH_SAMD) && defined(SEEED_GROVE_UI_WIRELESS)
+#include <Seeed_FS.h>
+#include <SD/Seeed_SD.h>
+#elif defined(ESP32)
+#include <SPIFFS.h>
+// #include <SD.h>
+#endif
+
+#include "MjpegClass.h"
+static MjpegClass mjpeg;
+
+/* variables */
+static int total_frames = 0;
+static unsigned long total_read_video = 0;
+static unsigned long total_decode_video = 0;
+static unsigned long total_show_video = 0;
+static unsigned long start_ms, curr_ms;
+
+// pixel drawing callback
+static void jpegDrawCallback(JPEGDRAW *pDraw)
+{
+  // Serial.printf("Draw pos = %d,%d. size = %d x %d\n", pDraw->x, pDraw->y, pDraw->iWidth, pDraw->iHeight);
+  unsigned long start = millis();
+  gfx->draw16bitBeRGBBitmap(pDraw->x, pDraw->y, pDraw->pPixels, pDraw->iWidth, pDraw->iHeight);
+  total_show_video += millis() - start;
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  // Init Display
+  gfx->begin();
+  gfx->fillScreen(BLACK);
+
+#ifdef TFT_BL
+  pinMode(TFT_BL, OUTPUT);
+  digitalWrite(TFT_BL, HIGH);
+#endif
+
+/* Wio Terminal */
+#if defined(ARDUINO_ARCH_SAMD) && defined(SEEED_GROVE_UI_WIRELESS)
+  // Init SPIFLASH
+  if (!SD.begin(SDCARD_SS_PIN, SDCARD_SPI, 4000000UL))
+#else
+  if (!SPIFFS.begin())
+  // if (!SD.begin())
+#endif
+  {
+    Serial.println(F("ERROR: SPIFFS Mount Failed!"));
+    gfx->println(F("ERROR: SPIFFS Mount Failed!"));
+  }
+  else
+  {
+#if defined(ARDUINO_ARCH_SAMD) && defined(SEEED_GROVE_UI_WIRELESS)
+    File vFile = SD.open(MJPEG_FILENAME, "r");
+#else
+    File vFile = SPIFFS.open(MJPEG_FILENAME, "r");
+    // File vFile = SD.open(MJPEG_FILENAME, "r");
+#endif
+
+    if (!vFile || vFile.isDirectory())
+    {
+      Serial.println(F("ERROR: Failed to open " MJPEG_FILENAME " file for reading"));
+      gfx->println(F("ERROR: Failed to open " MJPEG_FILENAME " file for reading"));
+    }
+    else
+    {
+      uint8_t *mjpeg_buf = (uint8_t *)malloc(MJPEG_BUFFER_SIZE);
+      if (!mjpeg_buf)
+      {
+        Serial.println(F("mjpeg_buf malloc failed!"));
+      }
+      else
+      {
+        Serial.println(F("MJPEG start"));
+
+        start_ms = millis();
+        curr_ms = millis();
+        mjpeg.setup(
+            &vFile, mjpeg_buf, jpegDrawCallback, true /* useBigEndian */,
+            0 /* x */, 0 /* y */, gfx->width() /* widthLimit */, gfx->height() /* heightLimit */);
+
+        while (vFile.available())
+        {
+          // Read video
+          mjpeg.readMjpegBuf();
+          total_read_video += millis() - curr_ms;
+          curr_ms = millis();
+
+          // Play video
+          mjpeg.drawJpg();
+          total_decode_video += millis() - curr_ms;
+
+          curr_ms = millis();
+          total_frames++;
+        }
+        int time_used = millis() - start_ms;
+        Serial.println(F("MJPEG end"));
+        vFile.close();
+        float fps = 1000.0 * total_frames / time_used;
+        total_decode_video -= total_show_video;
+        Serial.printf("Total frames: %d\n", total_frames);
+        Serial.printf("Time used: %d ms\n", time_used);
+        Serial.printf("Average FPS: %0.1f\n", fps);
+        Serial.printf("Read MJPEG: %lu ms (%0.1f %%)\n", total_read_video, 100.0 * total_read_video / time_used);
+        Serial.printf("Decode video: %lu ms (%0.1f %%)\n", total_decode_video, 100.0 * total_decode_video / time_used);
+        Serial.printf("Show video: %lu ms (%0.1f %%)\n", total_show_video, 100.0 * total_show_video / time_used);
+      }
+    }
+  }
+}
+
+void loop()
+{
+}
