0b3066473d7f6e8da385e94228347844bed921bf
moononournation <moononournation@gmail.com>
2021-03-07 21:41:39 +0800

    common dev device header file #24

--- examples/ImgViewerMjpeg/ImgViewerMjpeg.ino
+++ examples/ImgViewerMjpeg/ImgViewerMjpeg.ino
@@ -35,46 +35,8 @@
 // #define ESP32_LCDKIT_SPI
 // #define TTGO_T_DISPLAY
 // #define WT32_SC01
-#if defined(ESP32_LCDKIT_SPI)
-#define TFT_BL 23
-Arduino_DataBus *bus = new Arduino_ESP32SPI(19 /* DC */, 5 /* CS */, 22 /* SCK */, 21 /* MOSI */, 27 /* MISO */);
-Arduino_ILI9341 *gfx = new Arduino_ILI9341(bus, 18 /* RST */, 1 /* rotation */);
-
-#elif defined(TTGO_T_DISPLAY)
-#define TFT_BL 4
-Arduino_DataBus *bus = new Arduino_ESP32SPI(16 /* DC */, 5 /* CS */, 18 /* SCK */, 19 /* MOSI */, -1 /* MISO */);
-Arduino_ST7789 *gfx = new Arduino_ST7789(bus, 23 /* RST */, 2 /* rotation */, true /* IPS */, 135 /* width */, 240 /* height */, 53 /* col offset 1 */, 40 /* row offset 1 */, 52 /* col offset 2 */, 40 /* row offset 2 */);
-
-#elif defined(WT32_SC01)
-#define TFT_BL 23
-Arduino_DataBus *bus = new Arduino_ESP32SPI(21 /* DC */, 15 /* CS */, 14 /* SCK */, 13 /* MOSI */, -1 /* MISO */);
-Arduino_ST7796 *gfx = new Arduino_ST7796(bus, 22 /* RST */, 3 /* rotation */);
-
-/* Wio Terminal */
-#elif defined(ARDUINO_ARCH_SAMD) && defined(SEEED_GROVE_UI_WIRELESS)
-#define TFT_BL LCD_BACKLIGHT
-Arduino_HWSPI *bus = new Arduino_HWSPI(LCD_DC /* DC */, LCD_SS_PIN /* CS */);
-Arduino_ILI9341 *gfx = new Arduino_ILI9341(bus, -1 /* RST */, 3 /* rotation */);
-
-/* M5Stack */
-#elif defined(ARDUINO_M5Stack_Core_ESP32) || defined(ARDUINO_M5STACK_FIRE)
-#define TFT_BL 32
-Arduino_ESP32SPI *bus = new Arduino_ESP32SPI(27 /* DC */, 14 /* CS */, SCK, MOSI, MISO);
-Arduino_ILI9341_M5STACK *gfx = new Arduino_ILI9341_M5STACK(bus, 33 /* RST */, 1 /* rotation */);
-
-/* Odroid-Go */
-#elif defined(ARDUINO_ODROID_ESP32)
-#define TFT_BL 14
-Arduino_ESP32SPI *bus = new Arduino_ESP32SPI(21 /* DC */, 5 /* CS */, SCK, MOSI, MISO);
-Arduino_ILI9341 *gfx = new Arduino_ILI9341(bus, -1 /* RST */, 3 /* rotation */);
-// Arduino_ST7789 *gfx = new Arduino_ST7789(bus,  -1 /* RST */, 1 /* rotation */, true /* IPS */);
-
-/* TTGO T-Watch */
-#elif defined(ARDUINO_T) || defined(ARDUINO_TWATCH_BASE) || defined(ARDUINO_TWATCH_2020_V1) || defined(ARDUINO_TWATCH_2020_V2)
-#define TFT_BL 12
-Arduino_ESP32SPI *bus = new Arduino_ESP32SPI(27 /* DC */, 5 /* CS */, 18 /* SCK */, 19 /* MOSI */, -1 /* MISO */);
-Arduino_ST7789 *gfx = new Arduino_ST7789(bus, -1 /* RST */, 2 /* rotation */, true /* IPS */, 240, 240, 0, 80);
-
+#if defined(ESP32_LCDKIT_SPI) || defined(TTGO_T_DISPLAY) || defined(WT32_SC01) || (defined(ARDUINO_ARCH_SAMD) && defined(SEEED_GROVE_UI_WIRELESS)) || defined(ARDUINO_M5Stack_Core_ESP32) || defined(ARDUINO_M5STACK_FIRE) || defined(ARDUINO_ODROID_ESP32) || defined(ARDUINO_T) || defined(ARDUINO_TWATCH_BASE) || defined(ARDUINO_TWATCH_2020_V1) || defined(ARDUINO_TWATCH_2020_V2)
+#include <Arduino_GFX_auto_dev_device.h>
 #else /* not selected specific hardware */
 
 #if defined(ESP32)
