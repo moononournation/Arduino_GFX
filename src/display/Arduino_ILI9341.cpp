d96d9434898cfd2b4cfa2c2e3f33f121cedd5f99
moononournation <moononournation@gmail.com>
2021-05-05 20:48:36 +0800

    format

--- src/display/Arduino_ILI9341.cpp
+++ src/display/Arduino_ILI9341.cpp
@@ -118,7 +118,8 @@ void Arduino_ILI9341::tftInit()
 
   _bus->batchOperation(ili9341_init_operations, sizeof(ili9341_init_operations));
 
-  if (_ips) {
+  if (_ips)
+  {
     _bus->sendCommand(ILI9341_INVON);
   }
 }
