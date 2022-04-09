bfece780929fa7e019a07b56ed0119fe066b703c
moononournation <moononournation@gmail.com>
2022-04-09 15:05:25 +0800

    relocate U8g2 font files to common font folder

--- src/Arduino_GFX.h
+++ src/Arduino_GFX.h
@@ -21,6 +21,9 @@
 #if __has_include(<U8g2lib.h>)
 #include <U8g2lib.h>
 #define U8G2_FONT_SUPPORT
+#include "font/u8g2_font_unifont_h_utf8.h"
+#include "font/u8g2_font_unifont_t_chinese.h"
+#include "font/u8g2_font_unifont_t_cjk.h"
 #endif
 
 // Color definitions
