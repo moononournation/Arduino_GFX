4d74acb5a6af911faedb68acd675369d3334f1b6
moononournation <moononournation@gmail.com>
2022-04-11 22:49:38 +0800

    databus use SPI_DEFAULT_FREQ if not provided

--- src/Arduino_DataBus.h
+++ src/Arduino_DataBus.h
@@ -157,7 +157,7 @@ public:
 
     void unused() { UNUSED(_data16); } // avoid compiler warning
 
-    virtual void begin(int32_t speed, int8_t dataMode = GFX_NOT_DEFINED) = 0;
+    virtual void begin(int32_t speed = SPI_DEFAULT_FREQ, int8_t dataMode = GFX_NOT_DEFINED) = 0;
     virtual void beginWrite() = 0;
     virtual void endWrite() = 0;
     virtual void writeCommand(uint8_t c) = 0;
