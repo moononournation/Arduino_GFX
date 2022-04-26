92dea76a3134b2a96938f7e455f177f3b03be590
moononournation <moononournation@gmail.com>
2022-04-26 12:23:18 +0800

    avoid ESP32-S2 PSRAM pin

--- src/Arduino_GFX_Library.h
+++ src/Arduino_GFX_Library.h
@@ -140,14 +140,22 @@
 #define DF_GFX_DC 27
 #define DF_GFX_RST 33
 #define DF_GFX_BL 22
-#elif defined(ESP32) && (CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32S3)
+#elif defined(ESP32) && (CONFIG_IDF_TARGET_ESP32S2)
 #define DF_GFX_SCK 36
 #define DF_GFX_MOSI 35
 #define DF_GFX_MISO GFX_NOT_DEFINED
 #define DF_GFX_CS 34
-#define DF_GFX_DC 26
+#define DF_GFX_DC 35
 #define DF_GFX_RST 33
 #define DF_GFX_BL 21
+#elif defined(ESP32) && (CONFIG_IDF_TARGET_ESP32S3)
+#define DF_GFX_SCK 36
+#define DF_GFX_MOSI 35
+#define DF_GFX_MISO GFX_NOT_DEFINED
+#define DF_GFX_CS 44
+#define DF_GFX_DC 43
+#define DF_GFX_RST 42
+#define DF_GFX_BL 48
 #elif defined(ESP32) && (CONFIG_IDF_TARGET_ESP32C3)
 #define DF_GFX_SCK 4
 #define DF_GFX_MOSI 6
