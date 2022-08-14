c003b935c33435b20a2c339cd55ecc90af5c3bb5
moononournation <moononournation@gmail.com>
2022-08-14 18:05:15 +0800

    #183

--- src/display/Arduino_ST7735.cpp
+++ src/display/Arduino_ST7735.cpp
@@ -44,46 +44,56 @@ void Arduino_ST7735::tftInit()
     digitalWrite(_rst, HIGH);
     delay(ST7735_RST_DELAY);
   }
-  // else
-  // {
-  // Software Rest
-  _bus->sendCommand(ST7735_SWRESET); // 1: Software reset
-  delay(ST7735_RST_DELAY);
-  // }
+  else
+  {
+    // Software Rest
+    _bus->sendCommand(ST7735_SWRESET); // 1: Software reset
+    delay(ST7735_RST_DELAY);
+  }
 
-  _bus->sendCommand(ST7735_SLPOUT); //  2: Out of sleep mode, no args, w/delay
-  delay(ST7735_SLPOUT_DELAY);
-  _bus->sendCommand(ST7735_COLMOD); // 3: Set color mode, 1 arg + delay:
-  _bus->sendData(0x05);             // 16-bit color
+  uint8_t st7735_init_operations[] = {
+      BEGIN_WRITE,
+      WRITE_COMMAND_8, ST7735_SLPOUT, // 2: Out of sleep mode, no args, w/delay
+      END_WRITE,
+
+      DELAY, ST7735_SLPOUT_DELAY,
+
+      BEGIN_WRITE,
+      WRITE_C8_D8, ST7735_COLMOD, 0x05, // 3: Set color mode, 16-bit color
+
+      WRITE_COMMAND_8, ST7735_GMCTRP1, // Gamma Adjustments (pos. polarity), 16 args:
+      WRITE_BYTES, 16,
+      0x09, 0x16, 0x09, 0x20, // (Not entirely necessary, but provides
+      0x21, 0x1B, 0x13, 0x19, //  accurate colors)
+      0x17, 0x15, 0x1E, 0x2B,
+      0x04, 0x05, 0x02, 0x0E,
+
+      WRITE_COMMAND_8, ST7735_GMCTRN1, // Gamma Adjustments (neg. polarity), 16 args:
+      WRITE_BYTES, 16,
+      0x0B, 0x14, 0x08, 0x1E, // (Not entirely necessary, but provides
+      0x22, 0x1D, 0x18, 0x1E, //  accurate colors)
+      0x1B, 0x1A, 0x24, 0x2B,
+      0x06, 0x06, 0x02, 0x0F,
+      END_WRITE,
+
+      DELAY, 10,
+
+      BEGIN_WRITE,
+      WRITE_COMMAND_8, ST7735_NORON, // 5: Normal display on, no args, w/delay
+      END_WRITE,
+
+      DELAY, 10,
+
+      BEGIN_WRITE,
+      WRITE_COMMAND_8, ST7735_DISPON, // 6: Main screen turn on, no args, w/delay
+      END_WRITE};
+
+  _bus->batchOperation(st7735_init_operations, sizeof(st7735_init_operations));
+
   if (_ips)
   {
     _bus->sendCommand(ST7735_INVON);
   }
-
-  uint8_t gamma[] = {
-    BEGIN_WRITE,
-    WRITE_COMMAND_8, ST7735_GMCTRP1,  // Gamma Adjustments (pos. polarity), 16 args:
-    WRITE_DATA_8, 16,
-    0x09, 0x16, 0x09, 0x20,           // (Not entirely necessary, but provides
-    0x21, 0x1B, 0x13, 0x19,           //  accurate colors)
-    0x17, 0x15, 0x1E, 0x2B,
-    0x04, 0x05, 0x02, 0x0E,
-    END_WRITE,
-    BEGIN_WRITE,
-    WRITE_COMMAND_8, ST7735_GMCTRN1,  // Gamma Adjustments (neg. polarity), 16 args:
-    WRITE_DATA_8, 16,
-    0x0B, 0x14, 0x08, 0x1E,           // (Not entirely necessary, but provides
-    0x22, 0x1D, 0x18, 0x1E,           //  accurate colors)
-    0x1B, 0x1A, 0x24, 0x2B,
-    0x06, 0x06, 0x02, 0x0F,
-    END_WRITE,
-    DELAY, 10
-  };
-  _bus->batchOperation(gamma, sizeof(gamma)); // 4: Gamma correction
-
-  _bus->sendCommand(ST7735_NORON);  // 5: Normal display on, no args, w/delay
-  delay(10);
-  _bus->sendCommand(ST7735_DISPON); // 6: Main screen turn on, no args, w/delay
 }
 
 void Arduino_ST7735::writeAddrWindow(int16_t x, int16_t y, uint16_t w, uint16_t h)
