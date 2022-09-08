012f1bb96bdc72a51ed5ea8b873f238a77a4f01b
moononournation <moononournation@gmail.com>
2022-09-08 21:57:24 +0800

    add ILI6485 display

--- examples/PDQgraphicstest/PDQgraphicstest.ino
+++ examples/PDQgraphicstest/PDQgraphicstest.ino
@@ -46,10 +46,12 @@ Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
     5 /* G0 */, 6 /* G1 */, 7 /* G2 */, 15 /* G3 */, 16 /* G4 */, 4 /* G5 */,
     8 /* B0 */, 3 /* B1 */, 46 /* B2 */, 9 /* B3 */, 1 /* B4 */
 );
+// ILI6485 LCD 480x272
 Arduino_RPi_DPI_RGBPanel *gfx = new Arduino_RPi_DPI_RGBPanel(
   bus,
   480 /* width */, 0 /* hsync_polarity */, 8 /* hsync_front_porch */, 4 /* hsync_pulse_width */, 43 /* hsync_back_porch */,
-  272 /* height */, 0 /* vsync_polarity */, 8 /* vsync_front_porch */, 4 /* vsync_pulse_width */, 12 /* vsync_back_porch */);
+  272 /* height */, 0 /* vsync_polarity */, 8 /* vsync_front_porch */, 4 /* vsync_pulse_width */, 12 /* vsync_back_porch */,
+  1 /* pclk_active_neg */, 9000000 /* prefer_speed */, true /* auto_flush */);
 
 #elif defined(ESP32_LCDKIT_SPI)
 #define TFT_BL 23
