ac01d3e51ff31a4a6c38bbfc425176e1a8d4334c
moononournation <moononournation@gmail.com>
2023-10-20 21:24:28 +0800

    add option 5: 3.5" display

--- examples/PDQgraphicstest/Arduino_GFX_dev_device.h
+++ examples/PDQgraphicstest/Arduino_GFX_dev_device.h
@@ -256,6 +256,20 @@ Arduino_RGB_Display *gfx = new Arduino_RGB_Display(
 //     480 /* width */, 360 /* height */, rgbpanel, 0 /* rotation */, true /* auto_flush */,
 //     bus, GFX_NOT_DEFINED /* RST */, st7701_type8_init_operations, sizeof(st7701_type8_init_operations));
 
+// option 5:
+// Uncomment for 3.5" display
+// Arduino_ESP32RGBPanel *rgbpanel = new Arduino_ESP32RGBPanel(
+//     18 /* DE */, 17 /* VSYNC */, 16 /* HSYNC */, 21 /* PCLK */,
+//     4 /* R0 */, 3 /* R1 */, 2 /* R2 */, 1 /* R3 */, 0 /* R4 */,
+//     10 /* G0 */, 9 /* G1 */, 8 /* G2 */, 7 /* G3 */, 6 /* G4 */, 5 /* G5 */,
+//     15 /* B0 */, 14 /* B1 */, 13 /* B2 */, 12 /* B3 */, 11 /* B4 */,
+//     1 /* hsync_polarity */, 20 /* hsync_front_porch */, 30 /* hsync_pulse_width */, 38 /* hsync_back_porch */,
+//     1 /* vsync_polarity */, 4 /* vsync_front_porch */, 3 /* vsync_pulse_width */, 15 /* vsync_back_porch */,
+//     10, 16000000);
+// Arduino_RGB_Display *gfx = new Arduino_RGB_Display(
+//     640 /* width */, 480 /* height */, rgbpanel, 0 /* rotation */, true /* auto_flush */,
+//     bus, GFX_NOT_DEFINED /* RST */, st7701_type1_init_operations, sizeof(st7701_type1_init_operations));
+
 #elif defined(ESP32_S3_RPI_DPI)
 #define GFX_DEV_DEVICE ESP32_S3_RPI_DPI
 // #define GFX_BL 38
