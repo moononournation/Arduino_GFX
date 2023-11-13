ae10efa3633e503ffb31c84fd82d183542bfd93a
moononournation <moononournation@gmail.com>
2023-11-13 21:26:23 +0800

    add LilyGo T-Display-S3-Pro support

--- examples/PDQgraphicstest/Arduino_GFX_dev_device.h
+++ examples/PDQgraphicstest/Arduino_GFX_dev_device.h
@@ -21,6 +21,7 @@
 // #define LILYGO_T_DISPLAY
 // #define LILYGO_T_DISPLAY_S3
 // #define LILYGO_T_Display_S3_AMOLED
+// #define LILYGO_T_DISPLAY_S3_PRO
 // #define LILYGO_T_QT
 // #define LILYGO_T_RGB
 // #define LILYGO_T_TRACK
@@ -364,6 +365,12 @@ Arduino_GFX *gfx = new Arduino_RM67162(bus, 17 /* RST */, 0 /* rotation */);
     }
 Arduino_DataBus *bus = new Arduino_ESP32SPI(6 /* DC */, 5 /* CS */, 3 /* SCK */, 2 /* MOSI */, GFX_NOT_DEFINED /* MISO */);
 Arduino_GFX *gfx = new Arduino_GC9107(bus, 1 /* RST */, 0 /* rotation */, true /* IPS */);
+
+#elif defined(LILYGO_T_DISPLAY_S3_PRO)
+#define GFX_DEV_DEVICE LILYGO_T_DISPLAY_S3_PRO
+#define GFX_BL 48
+Arduino_DataBus *bus = new Arduino_ESP32SPI(9 /* DC */, 39 /* CS */, 18 /* SCK */, 17 /* MOSI */, 8 /* MISO */);
+Arduino_GFX *gfx = new Arduino_ST7796(bus, 47 /* RST */, 0 /* rotation */, true /* IPS */, 222 /* width */, 480 /* height */, 49 /* col offset 1 */, 0 /* row offset 1 */, 49 /* col offset 2 */, 0 /* row offset 2 */);
 
 #elif defined(LILYGO_T_RGB)
 #define GFX_DEV_DEVICE LILYGO_T_RGB
