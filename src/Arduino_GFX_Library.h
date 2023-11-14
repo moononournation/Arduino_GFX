e6da62d78ddcba298ceb2f5aedb74e7ada02468b
MakerM0 <jiangmin12youxiang@163.com>
2023-11-14 15:12:38 +0800

    add display nv3023 128x128

--- src/Arduino_GFX_Library.h
+++ src/Arduino_GFX_Library.h
@@ -72,6 +72,7 @@
 #include "display/Arduino_NT35310.h"
 #include "display/Arduino_NT35510.h"
 #include "display/Arduino_NT39125.h"
+#include "display/Arduino_NV3023.h"
 #include "display/Arduino_NV3041A.h"
 #include "display/Arduino_OTM8009A.h"
 #include "display/Arduino_R61529.h"
