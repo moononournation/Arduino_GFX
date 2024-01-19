923e65ece00fe0c75e415633ae34c516e674752e
Matthias Hertel <mathertel@hotmail.com>
2024-01-19 17:40:38 +0100

    SSD1306 + Arduino_Wire databus

--- src/display/Arduino_SSD1306.cpp
+++ src/display/Arduino_SSD1306.cpp
@@ -70,31 +70,28 @@ bool Arduino_SSD1306::begin(int32_t speed)
 
   static const uint8_t init_sequence[] = {
       BEGIN_WRITE,
-      WRITE_BYTES, 25,
-      SSD1306_DISPLAYOFF,                          // 0xAE
-      SSD1306_SETCONTRAST, _contrast,              // 0x81 nn
-      SSD1306_NORMALDISPLAY,                       // 0xA6
-      SSD1306_DEACTIVATE_SCROLL,                   // 0x2E
-      SSD1306_MEMORYMODE, 0x00,                    // 0x20 00 Horizontal addressing mode
-      SSD1306_SEGREMAPINV,                         // 0xA1
-      SSD1306_SETMULTIPLEX, (uint8_t)(HEIGHT - 1), // 0xA8 nn
-      SSD1306_COMSCANDEC,                          // 0xC8
-      SSD1306_SETDISPLAYOFFSET, 0x00,              // 0xD3 00   no offset
-      SSD1306_SETCOMPINS, comPins,                 // 0xDA nn
-      SSD1306_SETDISPLAYCLOCKDIV, 0x80,            // 0xD5 0x80
-      SSD1306_SETPRECHARGE, 0x22,                  // 0xd9 0x22
-      SSD1306_SETVCOMDETECT, 0x40,                 // 0xDB 0x40
-      SSD1306_CHARGEPUMP, 0x14,                    // 0x8D 0x14
-      SSD1306_SETSTARTLINE | 0x0,                  // 0x40       line #0
-      SSD1306_DISPLAYALLON_RESUME,                 // 0xA4
+      WRITE_COMMAND_8, SSD1306_DISPLAYOFF,                           // 0xAE
+      WRITE_C8_D8, SSD1306_SETCONTRAST, _contrast,              // 0x81 nn
+      WRITE_COMMAND_8, SSD1306_NORMALDISPLAY,                        // 0xA6
+      WRITE_COMMAND_8, SSD1306_DEACTIVATE_SCROLL,                    // 0x2E
+      WRITE_C8_D8, SSD1306_MEMORYMODE, 0x00,                    // 0x20 00 Horizontal addressing mode
+      WRITE_COMMAND_8, SSD1306_SEGREMAPINV,                          // 0xA1
+      WRITE_C8_D8, SSD1306_SETMULTIPLEX, (uint8_t)(HEIGHT - 1), // 0xA8 nn
+      WRITE_COMMAND_8, SSD1306_COMSCANDEC,                           // 0xC8
+      WRITE_C8_D8, SSD1306_SETDISPLAYOFFSET, 0x00,              // 0xD3 00   no offset
+      WRITE_C8_D8, SSD1306_SETCOMPINS, comPins,                 // 0xDA nn
+      WRITE_C8_D8, SSD1306_SETDISPLAYCLOCKDIV, 0x80,            // 0xD5 0x80
+      WRITE_C8_D8, SSD1306_SETPRECHARGE, 0x22,                  // 0xd9 0x22
+      WRITE_C8_D8, SSD1306_SETVCOMDETECT, 0x40,                 // 0xDB 0x40
+      WRITE_C8_D8, SSD1306_CHARGEPUMP, 0x14,                    // 0x8D 0x14
+      WRITE_COMMAND_8, SSD1306_SETSTARTLINE | 0x0,                   // 0x40       line #0
+      WRITE_COMMAND_8, SSD1306_DISPLAYALLON_RESUME,                  // 0xA4
       END_WRITE,
 
       DELAY, 100,
 
       BEGIN_WRITE,
-      WRITE_BYTES, 2,
-      0x00, // Co = 0, D/C = 0
-      SSD1306_DISPLAYON,
+      WRITE_COMMAND_8, SSD1306_DISPLAYON, // 0xAF
       END_WRITE};
 
   _bus->batchOperation(init_sequence, sizeof(init_sequence));
@@ -112,9 +109,10 @@ void Arduino_SSD1306::setBrightness(uint8_t /* brightness */)
 void Arduino_SSD1306::drawBitmap(int16_t xStart, int16_t yStart, uint8_t *bitmap, int16_t w, int16_t h, uint16_t /* color */, uint16_t /* bg */)
 {
   printf("SSD1306::drawBitmap %d/%d w:%d h:%d\n", xStart, yStart, w, h);
-  uint16_t bufferLength = TWI_BUFFER_LENGTH;
+  uint16_t count = w * ((h + 7) / 8);
 
 #if 0
+  // text output
   // print bitmap on Serial log
   for (int y = 0; y < h; y++)
   {
@@ -135,52 +133,49 @@ void Arduino_SSD1306::drawBitmap(int16_t xStart, int16_t yStart, uint8_t *bitmap
   Serial.println();
 #endif
 
+
 #if 1
-  // transfer the whole bitmap
-  for (uint8_t p = 0; p < _pages; p++)
-  {
-    uint8_t *pptr = bitmap + (p * w); // page start pointer
-    uint16_t bytesOut = 0;
+  // I2C version
 
-    // start page sequence
-    _bus->beginWrite();
-    // ==> _bus->writeBytes( SSD1306_PAGEADDR);
+  // start page sequence
+  _bus->beginWrite();
+  // _bus->writeC8D16(SSD1306_PAGEADDR, (0x00 << 8) | 0xFF);
+  _bus->writeCommand(SSD1306_PAGEADDR);
+  _bus->write(0);    // Page start address
+  _bus->write(0xFF); // Page end (not really, but works here)
 
-    _bus->write(SSD1306_PAGEADDR);
-    _bus->write(0);   // Page start address
-    _bus->write(0x7); // Page end (not really, but works here)
+  // _bus->writeC8D16(SSD1306_COLUMNADDR, (_colStart << 8) | _colEnd);
+  _bus->writeCommand(SSD1306_COLUMNADDR);
+  _bus->write(_colStart);
+  _bus->write(_colEnd);
+  _bus->endWrite();
 
-    _bus->write(SSD1306_COLUMNADDR);
-    _bus->write(_colStart);
-    _bus->write(_colEnd);
-    // set column
-    _bus->endWrite();
+  _bus->beginWrite();
+  _bus->writeBytes(bitmap, count);
+  _bus->endWrite();
 
-    // send out page data
-    for (int x = 0; x < w; x++)
-    {
-      if (!bytesOut)
-      {
-        _bus->beginWrite();
-        _bus->write((uint8_t)0x40);
-        bytesOut = 1;
-      }
-
-      _bus->write(*pptr++);
-      bytesOut++;
-
-      if (bytesOut == bufferLength)
-      {
-        _bus->endWrite();
-        bytesOut = 0;
-      }
-    }
-    if (bytesOut)
-    {
-      _bus->endWrite();
-    }
-  }
 #endif
+
+#if 0
+  // SPI version
+
+  _bus->beginWrite();
+
+  digitalWrite(D1, LOW);
+  _bus->write(SSD1306_PAGEADDR);
+  _bus->write(0x00);
+  _bus->write(0xFF);
+  _bus->write(SSD1306_COLUMNADDR);
+  _bus->write(_colStart);
+  _bus->write(_colEnd);
+  digitalWrite(D1, HIGH);
+  _bus->endWrite();
+
+  _bus->beginWrite();
+  _bus->writeBytes(bitmap, count);
+  _bus->endWrite();
+#endif
+
 } // drawBitmap()
 
 void Arduino_SSD1306::drawIndexedBitmap(int16_t, int16_t, uint8_t *, uint16_t *, int16_t, int16_t, int16_t)
@@ -210,22 +205,10 @@ void Arduino_SSD1306::invertDisplay(bool)
 
 void Arduino_SSD1306::displayOn(void)
 {
-  uint8_t seq[] = {
-      BEGIN_WRITE,
-      WRITE_BYTES, 2,
-      0x00, // sequence of commands
-      SSD1306_DISPLAYON,
-      END_WRITE};
-  _bus->batchOperation(seq, sizeof(seq));
+  _bus->sendCommand(SSD1306_DISPLAYON);
 }
 
 void Arduino_SSD1306::displayOff(void)
 {
-  uint8_t seq[] = {
-      BEGIN_WRITE,
-      WRITE_BYTES, 2,
-      0x00, // sequence of commands
-      SSD1306_DISPLAYOFF,
-      END_WRITE};
-  _bus->batchOperation(seq, sizeof(seq));
+  _bus->sendCommand(SSD1306_DISPLAYOFF);
 }
