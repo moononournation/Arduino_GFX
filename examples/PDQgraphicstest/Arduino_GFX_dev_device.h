1ea5a125ef0377cd8fc8a76f7f1a8a184998f2b3
Gilson Figueiredo <gilson.figueiredo.gz@gmail.com>
2024-04-23 13:10:46 -0300

    - Added new st7701 type for GUITION ESP32-4848S040 display board.

--- examples/PDQgraphicstest/Arduino_GFX_dev_device.h
+++ examples/PDQgraphicstest/Arduino_GFX_dev_device.h
@@ -291,6 +291,24 @@ Arduino_RGB_Display *gfx = new Arduino_RGB_Display(
 //     640 /* width */, 480 /* height */, rgbpanel, 0 /* rotation */, true /* auto_flush */,
 //     bus, GFX_NOT_DEFINED /* RST */, st7701_type1_init_operations, sizeof(st7701_type1_init_operations));
 
+// option 6:
+// Uncomment for 4.0" 86BOX from GUITION ESP32-4848S040 display
+// #define GFX_BL 38
+// 	Arduino_DataBus *bus = new Arduino_SWSPI(
+// 		GFX_NOT_DEFINED /* DC */, 39 /* CS */,
+// 		48 /* SCK */, 47 /* MOSI */, GFX_NOT_DEFINED /* MISO */);
+
+// 	Arduino_ESP32RGBPanel *rgbpanel = new Arduino_ESP32RGBPanel(
+// 		18 /* DE */, 17 /* VSYNC */, 16 /* HSYNC */, 21 /* PCLK */,
+// 		11 /* R0 */, 12 /* R1 */, 13 /* R2 */, 14 /* R3 */, 0 /* R4 */,
+// 		8 /* G0 */, 20 /* G1 */, 3 /* G2 */, 46 /* G3 */, 9 /* G4 */, 10 /* G5 */,
+// 		4 /* B0 */, 5 /* B1 */, 6 /* B2 */, 7 /* B3 */, 15 /* B4 */,
+// 		1 /* hsync_polarity */, 10 /* hsync_front_porch */, 8 /* hsync_pulse_width */, 50 /* hsync_back_porch */,
+// 		1 /* vsync_polarity */, 10 /* vsync_front_porch */, 8 /* vsync_pulse_width */, 20 /* vsync_back_porch */);
+// 	Arduino_RGB_Display *gfx = new Arduino_RGB_Display(
+// 		480 /* width */, 480 /* height */, rgbpanel, 1 /* rotation */, true /* auto_flush */,
+// 		bus, GFX_NOT_DEFINED /* RST */, st7701_type9_init_operations, sizeof(st7701_type9_init_operations));
+
 #elif defined(ESP32_S3_RPI_DPI)
 #define GFX_DEV_DEVICE ESP32_S3_RPI_DPI
 // #define GFX_BL 38
