72f56a2dd03bfc45f02d4007d561f6c6a3d7da51
moononournation <moononournation@gmail.com>
2024-11-03 14:15:17 +0800

    combine code in different ESP_ARDUINO_VERSION_MAJOR

--- src/databus/Arduino_ESP32RGBPanel.h
+++ src/databus/Arduino_ESP32RGBPanel.h
@@ -18,12 +18,17 @@
 #include "Arduino_DataBus.h"
 
 #if defined(ESP32) && (CONFIG_IDF_TARGET_ESP32S3)
-#if (!defined(ESP_ARDUINO_VERSION_MAJOR)) || (ESP_ARDUINO_VERSION_MAJOR < 3)
 
-#include "esp_lcd_panel_io.h"
 #include "esp_lcd_panel_rgb.h"
-#include "esp_lcd_panel_vendor.h"
 #include "esp_lcd_panel_ops.h"
+
+#include "esp32s3/rom/cache.h"
+// This function is located in ROM (also see esp_rom/${target}/ld/${target}.rom.ld)
+extern int Cache_WriteBack_Addr(uint32_t addr, uint32_t size);
+
+#if (!defined(ESP_ARDUINO_VERSION_MAJOR)) || (ESP_ARDUINO_VERSION_MAJOR < 3)
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_vendor.h"
 #include "esp_lcd_panel_interface.h"
 #include "esp_private/gdma.h"
 #include "esp_pm.h"
@@ -32,9 +37,6 @@
 #include "hal/lcd_hal.h"
 #include "hal/lcd_ll.h"
 
-#include "esp32s3/rom/cache.h"
-// This function is located in ROM (also see esp_rom/${target}/ld/${target}.rom.ld)
-extern int Cache_WriteBack_Addr(uint32_t addr, uint32_t size);
 
 // extract from esp-idf esp_lcd_rgb_panel.c
 struct esp_rgb_panel_t
@@ -67,6 +69,7 @@ struct esp_rgb_panel_t
   } flags;
   dma_descriptor_t dma_nodes[]; // DMA descriptor pool of size `num_dma_nodes`
 };
+#endif // #if (!defined(ESP_ARDUINO_VERSION_MAJOR)) || (ESP_ARDUINO_VERSION_MAJOR < 3)
 
 class Arduino_ESP32RGBPanel
 {
@@ -107,60 +110,6 @@ private:
   uint16_t _pclk_idle_high;
 
   esp_lcd_panel_handle_t _panel_handle = NULL;
-  esp_rgb_panel_t *_rgb_panel;
 };
 
-#else
-
-// Implementation for ESP32 board version 3.x
-// no need to include a copy of core esp32 types any more.
-
-#include "esp_lcd_panel_rgb.h"
-#include "esp_lcd_panel_ops.h"
-
-#include "esp32s3/rom/cache.h"
-// This function is located in ROM (also see esp_rom/${target}/ld/${target}.rom.ld)
-extern int Cache_WriteBack_Addr(uint32_t addr, uint32_t size);
-
-class Arduino_ESP32RGBPanel
-{
-public:
-  Arduino_ESP32RGBPanel(
-      int8_t de, int8_t vsync, int8_t hsync, int8_t pclk,
-      int8_t r0, int8_t r1, int8_t r2, int8_t r3, int8_t r4,
-      int8_t g0, int8_t g1, int8_t g2, int8_t g3, int8_t g4, int8_t g5,
-      int8_t b0, int8_t b1, int8_t b2, int8_t b3, int8_t b4,
-      uint16_t hsync_polarity, uint16_t hsync_front_porch, uint16_t hsync_pulse_width, uint16_t hsync_back_porch,
-      uint16_t vsync_polarity, uint16_t vsync_front_porch, uint16_t vsync_pulse_width, uint16_t vsync_back_porch,
-      uint16_t pclk_active_neg = 0, int32_t prefer_speed = GFX_NOT_DEFINED, bool useBigEndian = false,
-      uint16_t de_idle_high = 0, uint16_t pclk_idle_high = 0);
-
-  bool begin(int32_t speed = GFX_NOT_DEFINED);
-
-  uint16_t *getFrameBuffer(int16_t w, int16_t h);
-
-protected:
-private:
-  int32_t _speed;
-  int8_t _de, _vsync, _hsync, _pclk;
-  int8_t _r0, _r1, _r2, _r3, _r4;
-  int8_t _g0, _g1, _g2, _g3, _g4, _g5;
-  int8_t _b0, _b1, _b2, _b3, _b4;
-  uint16_t _hsync_polarity;
-  uint16_t _hsync_front_porch;
-  uint16_t _hsync_pulse_width;
-  uint16_t _hsync_back_porch;
-  uint16_t _vsync_polarity;
-  uint16_t _vsync_front_porch;
-  uint16_t _vsync_pulse_width;
-  uint16_t _vsync_back_porch;
-  uint16_t _pclk_active_neg;
-  int32_t _prefer_speed;
-  uint16_t _de_idle_high;
-  uint16_t _pclk_idle_high;
-
-  esp_lcd_panel_handle_t _panel_handle = NULL;
-};
-
-#endif // #if (!defined(ESP_ARDUINO_VERSION_MAJOR)) || (ESP_ARDUINO_VERSION_MAJOR < 3)
 #endif // #if defined(ESP32) && (CONFIG_IDF_TARGET_ESP32S3)
