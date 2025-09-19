abcdc6980c5f85d971460d25147eff5577d86a1e
moononournation <moononournation@gmail.com>
2025-09-19 23:06:57 +0800

    add st77916_150_init_operations

--- src/display/Arduino_ST77916.cpp
+++ src/display/Arduino_ST77916.cpp
@@ -3,8 +3,10 @@
 Arduino_ST77916::Arduino_ST77916(
     Arduino_DataBus *bus, int8_t rst, uint8_t r,
     bool ips, int16_t w, int16_t h,
-    uint8_t col_offset1, uint8_t row_offset1, uint8_t col_offset2, uint8_t row_offset2)
-    : Arduino_TFT(bus, rst, r, ips, w, h, col_offset1, row_offset1, col_offset2, row_offset2)
+    uint8_t col_offset1, uint8_t row_offset1, uint8_t col_offset2, uint8_t row_offset2,
+    const uint8_t *init_operations, size_t init_operations_len)
+    : Arduino_TFT(bus, rst, r, ips, w, h, col_offset1, row_offset1, col_offset2, row_offset2),
+      _init_operations(init_operations), _init_operations_len(init_operations_len)
 {
 }
 
@@ -101,7 +103,7 @@ void Arduino_ST77916::tftInit()
     delay(ST77916_RST_DELAY);
   }
 
-  _bus->batchOperation(st77916_init_operations, sizeof(st77916_init_operations));
+  _bus->batchOperation(_init_operations, _init_operations_len);
 
   invertDisplay(false);
 }
