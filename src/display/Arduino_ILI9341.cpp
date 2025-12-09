18c4604902867fab51c03429085844a00b9b536a
moononournation <moononournation@gmail.com>
2025-12-09 22:37:31 +0800

    #744

--- src/display/Arduino_ILI9341.cpp
+++ src/display/Arduino_ILI9341.cpp
@@ -6,8 +6,13 @@
 #include "Arduino_ILI9341.h"
 #include "SPI.h"
 
-Arduino_ILI9341::Arduino_ILI9341(Arduino_DataBus *bus, int8_t rst, uint8_t r, bool ips)
-    : Arduino_TFT(bus, rst, r, ips, ILI9341_TFTWIDTH, ILI9341_TFTHEIGHT, 0, 0, 0, 0)
+Arduino_ILI9341::Arduino_ILI9341(
+  Arduino_DataBus *bus, int8_t rst, uint8_t r,
+    bool ips, int16_t w, int16_t h,
+    uint8_t col_offset1, uint8_t row_offset1, uint8_t col_offset2, uint8_t row_offset2,
+    const uint8_t *init_operations, size_t init_operations_len)
+    : Arduino_TFT(bus, rst, r, ips, w, h, col_offset1, row_offset1, col_offset2, row_offset2),
+      _init_operations(init_operations), _init_operations_len(init_operations_len)
 {
 }
 
@@ -106,7 +111,7 @@ void Arduino_ILI9341::tftInit()
     delay(ILI9341_RST_DELAY);
   }
 
-  _bus->batchOperation(ili9341_init_operations, sizeof(ili9341_init_operations));
+  _bus->batchOperation(_init_operations, _init_operations_len);
 
   invertDisplay(false);
 }
--- src/display/Arduino_ILI9341.h
+++ src/display/Arduino_ILI9341.h
@@ -3,8 +3,7 @@
  * https://github.com/adafruit/Adafruit-GFX-Library.git
  * https://github.com/adafruit/Adafruit_ILI9341.git
  */
-#ifndef _ARDUINO_ILI9341_H_
-#define _ARDUINO_ILI9341_H_
+#pragma once
 
 #include "../Arduino_GFX.h"
 #include "../Arduino_TFT.h"
@@ -80,16 +79,16 @@
 #define ILI9341_MADCTL_BGR 0x08 ///< Blue-Green-Red pixel order
 #define ILI9341_MADCTL_MH 0x04  ///< LCD refresh right to left
 
-static const uint8_t ili9341_init_operations[] = {
+static const uint8_t ili9341_type1_init_operations[] = {
     BEGIN_WRITE,
-    WRITE_COMMAND_8, 0xCF,
-    WRITE_BYTES, 3, 0x00, 0xC1, 0x30,
-    WRITE_COMMAND_8, 0xED,
-    WRITE_BYTES, 4, 0x64, 0x03, 0x12, 0x81,
-    WRITE_COMMAND_8, 0xE8,
-    WRITE_BYTES, 3, 0x85, 0x00, 0x78,
-    WRITE_COMMAND_8, 0xCB,
-    WRITE_BYTES, 5, 0x39, 0x2C, 0x00, 0x34, 0x02,
+    WRITE_C8_BYTES, 0xCF, 3,
+    0x00, 0xC1, 0x30,
+    WRITE_C8_BYTES, 0xED, 4,
+    0x64, 0x03, 0x12, 0x81,
+    WRITE_C8_BYTES, 0xE8, 3,
+    0x85, 0x00, 0x78,
+    WRITE_C8_BYTES, 0xCB, 5,
+    0x39, 0x2C, 0x00, 0x34, 0x02,
     WRITE_C8_D8, 0xF7, 0x20,
     WRITE_C8_D16, 0xEA, 0x00, 0x00,
     WRITE_C8_D8, ILI9341_PWCTR1, 0x10,        // Power control VRH[5:0]
@@ -98,22 +97,48 @@ static const uint8_t ili9341_init_operations[] = {
     WRITE_C8_D8, ILI9341_VMCTR2, 0xB7,        // VCM control2
     WRITE_C8_D8, ILI9341_PIXFMT, 0x55,
     WRITE_C8_D16, ILI9341_FRMCTR1, 0x00, 0x1A,
-    WRITE_COMMAND_8, ILI9341_DFUNCTR, // Display Function Control
-    WRITE_BYTES, 3, 0x08, 0x82, 0x27,
+    WRITE_C8_BYTES, ILI9341_DFUNCTR, 3, // Display Function Control
+    0x08, 0x82, 0x27,
     WRITE_C8_D8, 0xF2, 0x00,             // 3Gamma Function Disable
     WRITE_C8_D8, ILI9341_GAMMASET, 0x01, // Gamma curve selected
-    // WRITE_COMMAND_8, ILI9341_GMCTRP1,    // Set Gamma
-    // WRITE_BYTES, 15,
-    // 0x0F, 0x31, 0x2B, 0x0C,
-    // 0x0E, 0x08, 0x4E, 0xF1,
-    // 0x37, 0x07, 0x10, 0x03,
-    // 0x0E, 0x09, 0x00,
-    // WRITE_COMMAND_8, ILI9341_GMCTRN1, // Set Gamma
-    // WRITE_BYTES, 15,
-    // 0x00, 0x0E, 0x14, 0x03,
-    // 0x11, 0x07, 0x31, 0xC1,
-    // 0x48, 0x08, 0x0F, 0x0C,
-    // 0x31, 0x36, 0x0F,
+    WRITE_COMMAND_8, ILI9341_SLPOUT,     // Exit Sleep
+    WRITE_COMMAND_8, ILI9341_DISPON,     // Display on
+    END_WRITE};
+
+static const uint8_t ili9341_type2_init_operations[] = {
+    BEGIN_WRITE,
+    WRITE_C8_BYTES, 0xEF, 3,
+    0x03, 0x80, 0x02,
+    WRITE_C8_BYTES, 0xCF, 3,
+    0x00, 0xC1, 0x30,
+    WRITE_C8_BYTES, 0xED, 4,
+    0x64, 0x03, 0x12, 0x81,
+    WRITE_C8_BYTES, 0xE8, 3,
+    0x85, 0x00, 0x78,
+    WRITE_C8_BYTES, 0xCB, 5,
+    0x39, 0x2C, 0x00, 0x34, 0x02,
+    WRITE_C8_D8, 0xF7, 0x20,
+    WRITE_C8_D16, 0xEA, 0x00, 0x00,
+    WRITE_C8_D8, ILI9341_PWCTR1, 0x23,        // Power control VRH[5:0]
+    WRITE_C8_D8, ILI9341_PWCTR2, 0x10,        // Power control SAP[2:0];BT[3:0]
+    WRITE_C8_D16, ILI9341_VMCTR1, 0x3e, 0x28, // VCM control
+    WRITE_C8_D8, ILI9341_VMCTR2, 0x86,        // VCM control2
+    WRITE_C8_D8, ILI9341_PIXFMT, 0x55,
+    WRITE_C8_D16, ILI9341_FRMCTR1, 0x00, 0x18,
+    WRITE_C8_BYTES, ILI9341_DFUNCTR, 3, // Display Function Control
+    0x08, 0x82, 0x27,
+    WRITE_C8_D8, 0xF2, 0x00,             // 3Gamma Function Disable
+    WRITE_C8_D8, ILI9341_GAMMASET, 0x01, // Gamma curve selected
+    WRITE_C8_BYTES, ILI9341_GMCTRP1, 15, // Set Gamma
+    0x0F, 0x31, 0x2B, 0x0C,
+    0x0E, 0x08, 0x4E, 0xF1,
+    0x37, 0x07, 0x10, 0x03,
+    0x0E, 0x09, 0x00,
+    WRITE_C8_BYTES, ILI9341_GMCTRN1, 15, // Set Gamma
+    0x00, 0x0E, 0x14, 0x03,
+    0x11, 0x07, 0x31, 0xC1,
+    0x48, 0x08, 0x0F, 0x0C,
+    0x31, 0x36, 0x0F,
     WRITE_COMMAND_8, ILI9341_SLPOUT, // Exit Sleep
     WRITE_COMMAND_8, ILI9341_DISPON, // Display on
     END_WRITE};
@@ -121,14 +146,15 @@ static const uint8_t ili9341_init_operations[] = {
 class Arduino_ILI9341 : public Arduino_TFT
 {
 public:
-  Arduino_ILI9341(Arduino_DataBus *bus, int8_t rst = GFX_NOT_DEFINED, uint8_t r = 0, bool ips = false);
+  Arduino_ILI9341(
+      Arduino_DataBus *bus, int8_t rst = GFX_NOT_DEFINED, uint8_t r = 0,
+      bool ips = false, int16_t w = ILI9341_TFTWIDTH, int16_t h = ILI9341_TFTHEIGHT,
+      uint8_t col_offset1 = 0, uint8_t row_offset1 = 0, uint8_t col_offset2 = 0, uint8_t row_offset2 = 0,
+      const uint8_t *init_operations = ili9341_type1_init_operations, size_t init_operations_len = sizeof(ili9341_type1_init_operations));
 
   bool begin(int32_t speed = GFX_NOT_DEFINED) override;
-
-  void setRotation(uint8_t r) override;
-
   void writeAddrWindow(int16_t x, int16_t y, uint16_t w, uint16_t h) override;
-
+  void setRotation(uint8_t r) override;
   void invertDisplay(bool) override;
   void displayOn() override;
   void displayOff() override;
@@ -137,6 +163,6 @@ protected:
   void tftInit() override;
 
 private:
+  const uint8_t *_init_operations;
+  size_t _init_operations_len;
 };
-
-#endif
