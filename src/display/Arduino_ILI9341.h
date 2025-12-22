8d5f33a762f8a02c562c1ac5f7589a7b2ce39a52
moononournation <moononournation@gmail.com>
2025-12-23 06:50:31 +0800

    #744

--- src/display/Arduino_ILI9341.h
+++ src/display/Arduino_ILI9341.h
@@ -143,6 +143,32 @@ static const uint8_t ili9341_type2_init_operations[] = {
     WRITE_COMMAND_8, ILI9341_DISPON, // Display on
     END_WRITE};
 
+static const uint8_t ili9341_type3_init_operations[] = {
+    BEGIN_WRITE,
+    WRITE_C8_BYTES, 0xCF, 3,
+    0x00, 0xC1, 0x30,
+    WRITE_C8_BYTES, 0xED, 4,
+    0x64, 0x03, 0x12, 0x81,
+    WRITE_C8_BYTES, 0xE8, 3,
+    0x85, 0x00, 0x78,
+    WRITE_C8_BYTES, 0xCB, 5,
+    0x39, 0x2C, 0x00, 0x34, 0x02,
+    WRITE_C8_D8, 0xF7, 0x20,
+    WRITE_C8_D16, 0xEA, 0x00, 0x00,
+    WRITE_C8_D8, ILI9341_PWCTR1, 0x10,        // Power control VRH[5:0]
+    WRITE_C8_D8, ILI9341_PWCTR2, 0x00,        // Power control SAP[2:0];BT[3:0]
+    WRITE_C8_D16, ILI9341_VMCTR1, 0x30, 0x30, // VCM control
+    WRITE_C8_D8, ILI9341_VMCTR2, 0x86,        // VCM control2
+    WRITE_C8_D8, ILI9341_PIXFMT, 0x55,
+    WRITE_C8_D16, ILI9341_FRMCTR1, 0x00, 0x1A,
+    WRITE_C8_BYTES, ILI9341_DFUNCTR, 3, // Display Function Control
+    0x08, 0x82, 0x27,
+    WRITE_C8_D8, 0xF2, 0x00,             // 3Gamma Function Disable
+    WRITE_C8_D8, ILI9341_GAMMASET, 0x01, // Gamma curve selected
+    WRITE_COMMAND_8, ILI9341_SLPOUT,     // Exit Sleep
+    WRITE_COMMAND_8, ILI9341_DISPON,     // Display on
+    END_WRITE};
+
 class Arduino_ILI9341 : public Arduino_TFT
 {
 public:
